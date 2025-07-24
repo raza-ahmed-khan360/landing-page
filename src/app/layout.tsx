3755ba4d45f5becb43a952a87bbc7ebaa05e67eb
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2025-07-24 10:15:14 +0500

    added types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import BackToTopButton from "./components/BackToTopButton";
 import Navbar from "./components/NavBar";
 import Footer from "./components/FooterSection";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Raza – Dev',
   description: "Welcome to Raza Ahmed's developer portfolio. Explore high-performance web projects, modern UI/UX design, animation-rich interfaces, and seamless digital solutions built with Next.js, Tailwind CSS, and Framer Motion.",
   openGraph: {
